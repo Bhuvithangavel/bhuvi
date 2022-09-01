88a6601418ea4a2cd90b9e435bbb334700457d27
Bhuvithangavel <102967627+Bhuvithangavel@users.noreply.github.com>
2022-09-01 17:39:12 +0530

    plan page

---
+++ plan.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="assets/img/favicon.ico">
+	<!-- Style CSS Start -->
+	<link type="text/css" href="assets/css/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap CSS -->	
+	<link type="text/css" href="assets/css/style.css" rel="stylesheet"><!-- Style CSS -->	
+	<link type="text/css" href="assets/css/menu.css" rel="stylesheet"><!-- Menu CSS -->	
+	<link type="text/css" href="assets/css/mediaquery.css" rel="stylesheet"><!-- Mediaquery CSS -->	
+	<link type="text/css" href="assets/fonts/font.css" rel="stylesheet"><!-- Font CSS -->	
+    <link type="text/css" href="assets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"><!-- Bootstrap Icons CSS -->
+	<link type="text/css" href="assets/css/all.min.css" rel="stylesheet"><!-- FontAwesome CSS -->	
+	<!-- Style CSS End -->
+	<title>LetsFAME</title>
+</head>
+<body>
+    <!-- ======= Header Start======= -->
+	<?php include 'header.php';?>
+	<!-- Header End  -->
+	
+	<!-- Choose your plan.-->
+<section class="premium " >
+    <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padz classific100media">
+                <div class="planapps">
+                    <img alt="letsfame" class="img-fluid" data-aos="zoom-in-up" src="assets/img/plan.png">
+                    <div class="access">
+                        <h1>Choose your plan.</h1>
+                        <h3 class="my-4">Unlimited Access.<br>For a whole year.</h3>
+                        <a class="subscribe-btn py-2" href="#"  > Get Started now</a>
+                        <h5 class="my-4">Plan auto renews for <span><img src="assets/img/rupee.svg" alt="premium"
+                                    class="img-fluid"></span>1,999/ year until cancelled.</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-5 classific100media">
+                
+                    <div class="pricing-lists">
+                        <ul class="ul-one">
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li>Access to connect</li>
+                            <li>Access to connect with everyone</li>
+                            <li>who viewed your profile - days</li>
+                            <li>Profile Reachability</li>
+                            <li>Get displayed first</li>
+                            <li>Suggestions only from verified profiles</li>
+                            <li>Apply for job Calls</li>
+                            <li>Post Job Calls</li>
+                            <li>Personalized Analytics for your profile</li>
+                            <li>Personalized Analytics for your Posts &amp; job Calls</li>
+                            <li>Promote your work for free on the platform</li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                        </ul>
+                        <ul class="ul-two">
+                            <li> 
+                                <div class="plan">
+                                    <p>Basic</p>
+                                    <h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>999/ year</h4>
+                                </div>      
+                                <!-- <h6>Basic</h6> 
+                                <span class="h6 fw-bold"><img src="assets/img/rupee.svg" alt="premium" class="img-fluid ">999/Year</span>  -->
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li>60</li>
+                            <li>2x</li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li><button class="platform-btn" >Get&nbspStarted</button> </li>
+                        </ul>
+                        <ul class="ul-three">
+                            <li>
+                                <div class="plan">
+                                    <p><span><img src="assets/img/premium.svg" alt="premium" class="prem"></span>Premium</p>
+                                    <h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>1999/ year</h4>
+                                    <h5>Recommended</h5>
+                                </div> 
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>90</li>
+                            <li>4x</li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>
+                                <h3>nodata</h3>
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>2 times</li>
+                            <li><button class="platform-btn" >Get&nbspStarted</button> </li>
+                        </ul>
+                        <ul class="ul-four">
+                            <li>
+                                <div class="plan">
+                                    <p><span><img src="assets/img/premiumpro.svg" alt="premium" class="prem"></span>Premium&nbspPro</p>
+                                    <h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>2999/ year</h4>
+                                </div>
+                            </li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>120</li>
+                            <li>6x</li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li><img alt="img" class="img-fluid" src="assets/img/tick.svg"></li>
+                            <li>4 times</li>
+                            <li><button class="platform-btn" >Get&nbspStarted</button> </li>
+                        </ul>
+                    </div>
+                   
+                        
+                
+            </div>
+         
+    </div>
+</section>
+ 
+  
+	
+	<!-- ======= Footer Start ======= -->
+	<?php include 'footer.php';?>
+	<!-- Footer End  -->
+	<!-- JS Start -->
+	<script src="assets/js/main.js"></script><!-- Menu JS -->
+	<script src="assets/js/wow.min.js"></script><!-- Menu Animation JS -->
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/bootstrap.bundle.min.js"></script><!-- Bootstrap Bundle JS -->
+	<script src="assets/js/all.min.js"></script><!-- FontAwesome JS -->
+	<!-- JS End -->
+</body>
+</html>
