4ed0818d4cdc59055de5ea6908f70e0fffbbdbfc
Bhuvithangavel <102967627+Bhuvithangavel@users.noreply.github.com>
2022-09-01 17:48:16 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,203 @@
+ 
+<section class="footer">
+		<div class="container py-5 " data-aos="zoom-in">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 ">
+							<div class="row">
+								<div class="col-lg-6 col-md-6 px-5 text-start classific100media">
+									<ul class="footlist">
+										<li><a href="#" data-bs-toggle="modal" data-bs-target="#login">Sign In</a>
+										</li>
+										<li><a href="#">Get&nbspStarted</a>
+										</li>
+										<li><a href="plan.php">Plans</a>
+										</li>
+										<li><a href="contactus.php">Contact&nbspUs</a>
+										</li>
+										<li><a href="#">User&nbspAgreement</a>
+										</li>
+										<li><a href="#">Privacy&nbspPolicy</a>
+										</li>
+										<li><a href="#">Cookie&nbspPolicy</a>
+										</li>
+										<li><a href="#" data-bs-toggle="modal" data-bs-target="#profile_confir">Profile&nbspConfirmation</a></li>
+										<li><a href="#" data-bs-toggle="modal" data-bs-target="#request_sent">Request&nbspSent</a></li>
+										<li><a href="#" data-bs-toggle="modal" data-bs-target="#professional">Professional  </a></li>
+										<li><a href="#" data-bs-toggle="modal" data-bs-target="#datepicker">Datepicker  </a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 px-4 classific100media">
+							<div class="connect">
+								<h2><span><img alt="letsfame" class="prempro" src="assets/img/premiumpro.svg"></span>Connecting Creative Minds</h2>
+								<p class="pl-5">Available for Download on the App Store and Google Play</p>
+							</div>
+							<div class="footer-app ">
+								<img src="assets/img/ios.svg" alt="img" class="img-fluid mt-3">
+								<img src="assets/img/android.svg" alt="img" class=" ms-2 img-fluid mt-3">
+							</div>
+							<div class="follow mt-3">Follow us on :
+								<span><a target="_blank" href="https://www.facebook.com/103476694501909/"><img class="socico" src="assets/img/fb.svg" alt="social"></a></span>
+								<span><a target="_blank" href="https://instagram.com/letsfameofficial?igshid=1pxnaoqri8oxb"><img class="socico" src="assets/img/insta.svg" alt="social"></a></span>
+								<span><a target="_blank" href="https://twitter.com/letsfameoffl?s=20"><img class="" style="width: 25px;" src="assets/img/youtube.svg" alt="social"></a></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="copysection center">
+		<p>Copyright © <span id="year">2022</span> Let's FAME. <span class="rights">All Rights Reserved.</span>
+		</p>
+	</section>
+
+
+
+<!-- ======= Profile Confirmation Modal Start======= -->
+<div class="modal fade" id="profile_confir" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header no-border">
+                <div class="close-btn"><button type="button" class="btn-close p-0" data-bs-dismiss="modal"
+                        aria-label="Close"></button></div>
+            </div>
+            <div class="modal-body p-0">
+                <div class="w-100 container">
+                    <div class="row"> 
+                        <div class="col-md-12">
+                            <div class="login-details mt-5 text-center"> 
+                                <h3 class="text-uppercase">Profile Confirmation</h3>
+                                <p class="mt-5">Other members can see your profile and can reach out to you,so make sure all the information you provided are authentic and relevant.</p>
+                                <p>By creating your profile,you become a registered user of LetsFAME and you agree to the <span class="yellow underline"><a href=""><u>Terms & Condition</u></a></span> </p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 offset-md-3 my-5 px-5"> 
+							<button class="form-btn text-uppercase" href="">Create Profile</button>
+							<button class="form-btn-cancel text-uppercase mt-3" href="">Cancel</button>
+						</div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer no-border">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Profile Confirmation Modal End  -->
+
+
+<!-- ======= Request Sent Modal Start======= -->
+<div class="modal fade" id="request_sent" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header no-border">
+                <div class="close-btn"><button type="button" class="btn-close p-0" data-bs-dismiss="modal"
+                        aria-label="Close"></button></div>
+            </div>
+            <div class="modal-body p-0 ">
+                <div class="w-100 container">
+                    <div class="row"> 
+                        <div class="col-md-12">
+                            <div class="login-details mt-5 text-center"> 
+                                <h3 class="text-uppercase">Request Sent</h3>
+                             
+                            <div class="bg-tick mt-4" align="center">		
+                            	<i class="bi bi-check2 px-4"></i> </div>
+                             
+                                <p class="mt-4">Thank you for submitting Your details with us. Our team will now review your identity and your profile dateils to get you verified.</p>
+                                <p>If you do not hear from us within 2 working days, please get in touch with us at</p> 
+                                <h5 class="yellow underline "><a href=""><u>support@letsfame.com </u></a></h5>   
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 offset-md-3 my-5  px-5">
+							<button class="form-btn text-uppercase" href="">ok</button> 
+						</div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer no-border">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Request Sent Modal End  -->
+
+<!-- ======= Professional Modal Start======= -->
+<div class="modal fade" id="professional" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header no-border">
+                <div class="close-btn"><button type="button" class="btn-close p-0" data-bs-dismiss="modal"
+                        aria-label="Close"></button></div>
+            </div>
+            <div class="modal-body p-0">
+                <div class="w-100">
+                    <div class="row">
+                        <div class="col-md-4 mob-form-image ">
+                            <img src="assets/img/professional-bg.png" alt="LetsFame" class="img-fluid bgimg  rounded-start">
+                        </div>
+                        <div class="col-md-8 ">
+                            <div class="login-details mt-5"> 
+                                <h3>#1 Professional online platform for the entertainment industry</h3>
+                                <hr>
+                                <h5 class="text-uppercase my-5 text-warning">Download our mobile apps to experience the full potential of the platform</h5>
+                                
+
+                                <div class="my-5">
+                                	<h6 class="mb-3">Downloads Available on</h6>
+									<img src="assets/img/ios.svg" alt="img" class="img-fluid ">
+									<img src="assets/img/android.svg" alt="img" class="img-fluid">
+								</div>	
+
+                                 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer no-border">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Professional Modal End  -->
+
+<!-- ======= Datepicker Modal Start======= -->
+<div class="modal fade" id="datepicker" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header no-border">
+                <div class="close-btn"><button type="button" class="btn-close p-0" data-bs-dismiss="modal"
+                        aria-label="Close"></button></div>
+            </div>
+            <div class="modal-body p-0">
+                <div class="w-100 container">
+                    <div class="row"> 
+                        <div class="col-md-12 p-5 m-5">
+ 
+
+<form action="/action_page.php"> 
+  <input type="date" id="birthday" name="birthday" placeholder="dd"> 
+</form>
+
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer no-border">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Datepicker Modal End  -->
+
+
+ 
