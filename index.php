8364257f412f046c9b770b246e4a6c1349835262
Bhuvithangavel <102967627+Bhuvithangavel@users.noreply.github.com>
2022-09-01 17:35:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="assets/img/favicon.ico">
+	<!-- Style CSS Start -->
+	<link type="text/css" href="assets/css/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap CSS -->	
+	<link type="text/css" href="assets/css/style.css" rel="stylesheet"><!-- Style CSS -->	
+	<link type="text/css" href="assets/css/menu.css" rel="stylesheet"><!-- Menu CSS -->	
+	<link type="text/css" href="assets/css/mediaquery.css" rel="stylesheet"><!-- Mediaquery CSS -->	
+	<link type="text/css" href="assets/fonts/font.css" rel="stylesheet"><!-- Font CSS -->	
+    <link type="text/css" href="assets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"><!-- Icons CSS -->
+	<link type="text/css" href="assets/css/all.min.css" rel="stylesheet"><!-- FontAwesome CSS -->	
+	<!-- Style CSS End -->
+	<title>LetsFAME</title>
+</head>
+<body>
+    <!-- ======= Header Start======= -->
+	<?php include 'header.php';?>
+	<!-- Header End  -->
+	<!-- ======= Banner Start ======= -->
+	<section>
+		<div class="banner">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 classific100media">
+						<div class="banner-content textstartmedia">
+							<div class="banner-logo pt-5 textcentermedia">
+								<img src="assets/img/head_logo.png" alt="Banner" class="img-fluid">
+							</div>
+							<h2 class="pt-3">#1 Professional online platform for the entertainment indurstry</h2>
+							<h5 class="fw-bold pt-3">Downloads available on</h5>
+							<div class="banner-app pt-3">
+								<a href="" ><img alt="letsfame" class="google" src="assets/img/ios.svg"></a>
+								<a href="" ><img alt="letsfame" class="google" src="assets/img/android.svg"></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 py-5 textendmedia classific100media"><img src="assets/img/phone.png" alt="Banner" class="img-fluid w-75 pt2media">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Banner End  -->
+	<!-- ======= Creative Studio Start ======= -->
+	<section>
+		<div class="creative-studio">
+			<img src="assets/img/bigestcut.png" alt="Banner" class="img-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="creative-content ">All the creative minds in one place
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Creative Studio End  -->
+	<!-- ======= Creative Profile Start ======= -->
+	<section>
+		<div class="creative-profile">
+			<img src="assets/img/downcut.png" alt="Banner" class="img-fluid">
+		</div>
+	</section>
+	<!-- Creative Profile End  -->
+	<!-- ======= Platform Start ======= -->
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="platform py-5">
+						<h2>First Platform exclusively for the entertainment industry.</h2>
+						<div class="list">
+							<p><span class="platform-icon"><img src="assets/img/cut.svg" alt="cut" class="img-fluid"></span>Connect with top industry personalities</p>
+							<p><span class="platform-icon"><img src="assets/img/bulb.svg" alt="bulb" class="img-fluid"></span>Find the best creative talents</p>
+							<p><span class="platform-icon"><img src="assets/img/connect.svg" alt="connect" class="img-fluid"></span>letsfameExpand your industry network</p>
+							<p><span class="platform-icon"><img src="assets/img/market.svg" alt="market" class="img-fluid"></span>Post and apply for job calls</p>
+						</div>
+						<button class="platform-btn" href="" target="_blank"> GET STARTED </button>
+					</div>
+				</div>
+				<div class="col-md-4 pt-5 text-center">
+					<div class="platform-image"><img src="assets/img/grey.svg" alt="grey" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Platform End  -->
+	<!-- ======= Flow Grid Start ======= -->
+	<section class="flow-grid py-5">
+		<h2>It's never been this easy before</h2>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone1.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75">Search with advanced filters</h5>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone2.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75">See who viewed your profile</h5>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone3.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75">Get a personalised user analytics dashboard </h5>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone4.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75">Get a personalised job dashboard</h5>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone5.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75">Post your job calls in the industry</h5>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="flow-grid-item py-3 mb-3">
+								<img src="assets/img/phone6.png" alt="Banner" class="img-fluid w-75">
+								<h5 class="w-75 text-center">Apply for job calls in the industry</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Flow Grid End  -->
+	<!-- ======= Profile Plan Start ======= -->
+	<section>
+		<div class="profile-plan classific100media">
+			<img src="assets/img/plans@2x.png" alt="Banner" class="img-fluid">
+			<div class="profile-plan-content">
+				<h2>Choose your plan.</h2>
+				<h5>Unlimited Access.</h5>
+				<h5>For a whole year.</h5>
+				<button class="subscribe-btn" href="" target="_blank"> GET STARTED NOW </button>
+				<p>Plan auto renews for <span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>1,999/ year until cancelled.</p>
+			</div>
+		</div>
+		<div class="premium-plan classific100media">
+			<div class="pricing-lists">
+				<ul class="ul-one">
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li>Access to connect</li>
+					<li>Access to connect with everyone</li>
+					<li>who viewed your profile - days</li>
+					<li>Profile Reachability</li>
+					<li>Get displayed first</li>
+					<li>Suggestions only from verified profiles</li>
+					<li>Apply for job Calls</li>
+					<li>Post Job Calls</li>
+					<li>Personalized Analytics for your profile</li>
+					<li>Personalized Analytics for your Posts &amp; job Calls</li>
+					<li>Promote your work for free on the platform</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+				</ul>
+				<ul class="ul-two">
+					<li>
+						<div class="plan">
+							<p>Basic</p>
+							<h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>999/ year</h4>
+						</div>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li>60</li>
+					<li>2x</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li>
+						<!-- href="https://form.typeform.com/to/ioxNrsxy?typeform-medium=embed-snippet" data-mode="popup" data-size="100" target="_blank" -->
+						<button class="platform-btn" >Get&nbspStarted</button> 
+					</li>
+				</ul>
+				<ul class="ul-three">
+					<li>
+						<div class="plan">
+							<p><span><img src="assets/img/premium.svg" alt="premium" class="prem"></span>Premium</p>
+							<h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>1999/ year</h4>
+							<h5>Recommended</h5>
+						</div>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>90</li>
+					<li>4x</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>
+						<h3>nodata</h3>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>2 times</li>
+					<li><button class="platform-btn " >Get&nbspStarted</button> 
+					</li>
+				</ul>
+				<ul class="ul-four">
+					<li>
+						<div class="plan">
+							<p><span><img src="assets/img/premiumpro.svg" alt="premium" class="prem"></span>Premium&nbspPro</p>
+							<h4><span><img src="assets/img/rupee.svg" alt="premium" class="rupee"></span>2999/ year</h4>
+						</div>
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>120</li>
+					<li>6x</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li><img alt="" class="" src="assets/img/tick.svg">
+					</li>
+					<li>4 times</li>
+					<li><button class="platform-btn" >Get&nbspStarted</button> 
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<!-- Profile Plan End  -->
+	<!-- ======= Footer Start ======= -->
+	<?php include 'footer.php';?>
+	<!-- Footer End  -->
+	<!-- JS Start -->
+	<script src="assets/js/jquery.min.js"></script><!-- Jquery Min JS -->
+	<script src="assets/js/bootstrap.bundle.min.js"></script><!-- Bootstrap Bundle JS -->
+	<script src="assets/js/all.min.js"></script><!-- FontAwesome JS -->
+	<!-- JS End -->
+</body>
+</html>
